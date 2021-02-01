d3b5ec87a6d28c82aff14c127a388175c25bf05b
Subrato Sarkar <subratosarkar@Subratos-MacBook-Pro.local>
2021-01-31 22:07:39 -0500

    updated code

--- config/groups.js
+++ config/groups.js
@@ -3,9 +3,9 @@
 const { checkout } = require("../data/data");
 
 module.exports = {
-    specs: {
-        all: './test/specs/**/*.js',
-        search: './test/specs/search.js',
-        checkout: './test/specs/checkout.js'
-    }
+    specs: [
+        './test/specs/**/*.js',
+        // './test/specs/search.js',
+        // './test/specs/checkout.js',
+    ]
 }
